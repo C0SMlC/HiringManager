5d79ea09b063cb0a16e18df9071b03d664d80437
patiltejass <tp291101@gmail.com>
2024-09-20 17:02:36 +0530

    Fix: Updated jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -56,7 +56,7 @@ router.post("/login", (req, res) => {
     const token = jwt.sign(
       { username: user.username, role: user.role },
       config.secret,
-      { expiresIn: "365d" }
+      { expiresIn: "8760h" }
     );
 
     // Update the loggedInTime in the database
