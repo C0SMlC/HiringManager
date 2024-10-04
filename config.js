9904aaa8d61efb024b1dd27709723d52d22b2877
dandela-ravi <ravi.dandela@deltaiot.in>
2024-10-04 15:25:19 +0530

    Updated

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  secret: "mydb", // Use a strong secret key
+  database: {
+    host: "localhost",
+    user: "root",
+    // password: 'Abcd@1234',
+    password: "Sa@7222345",
+    database: "ApplicantTracking",
+  },
+};
